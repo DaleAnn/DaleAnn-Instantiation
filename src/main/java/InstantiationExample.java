a91aa5e0e672b0dad85d19aa8029168e9be79c06
DaleAnn <daviddaleann@yahoo.com>
2023-01-31 18:34:47 +0000

    Done!

--- src/main/java/InstantiationExample.java
+++ src/main/java/InstantiationExample.java
@@ -18,6 +18,7 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Object ob = new Object();
+        return ob;
     }
 }
